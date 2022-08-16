0fff45419be4a7ae1234e17fba436a4978b3c043
ines <goudjilines25@gmail.com>
2022-08-16 22:42:39 +0100

    profil

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import NavBar from './comps/navBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/comps/navBar.js
@@ -0,0 +1,30 @@
+import './navBar.css';
+
+function NavBar() {
+    return (
+        <div className='container'>
+
+            <div className='espace'>
+            </div>
+
+            <h4>EUISMOD</h4>
+
+            <div className='bigTitle'>
+                <h1>Future Perfect</h1>
+            </div>
+
+            <hr></hr>
+
+            <div className='para'>
+                <p>
+                    Morbi quis commodo odio aenean sed. Sit amet consectetur adipiscing elit pellentesque habitant morbi sed et adipiscing.
+                </p>
+
+                <button>Learn More ➤</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default NavBar;
